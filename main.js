a1211f6633604533f20abdc168daffb6193bb6e0
Vlad <skodorvlad371@gmail.com>
2023-03-29 17:24:18 +0300

    'Finish'

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const burger = document.querySelectorAll('#burger');
 const menu = document.querySelectorAll('nav');
-burger[0].onclick = function() {
-	menu[0].classList.toggle('nav-action');
+
+burger[0].onclick = function () {
+    menu[0].classList.toggle('nav-action');
 };
+
+window.addEventListener('scroll', function(e) {
+    var header = document.querySelectorAll('header');
+    if(scrollY => 100) {
+    	header[0].style.transition = '.5s';
+        header[0].style.background = '#031921';
+    }
+    if(scrollY <= 100) {
+    	header[0].style.transition = '.5s';
+        header[0].style.background = 'none';
+    }
+});
+
